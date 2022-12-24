ed503f077f2dd9984377b4d7729f57ccfb6d92a7
Patrick <patrickducusin2@gmail.com>
2022-12-24 00:12:43 -0500

    Remove redundant specification of async redirect callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,19 +9,12 @@ import { prisma } from "../../../server/db/client";
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    async session({ session, user }) {
+    session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
       }
       return session;
     },
-    async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url
-      return baseUrl
-    }
   },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
