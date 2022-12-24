8ae615b04f9a3b3a0713b93633fc99b8da7d0382
Patrick <patrickducusin2@gmail.com>
2022-12-24 00:12:13 -0500

    In LoginForm, change button to not be of submit type which caused callbackURL errors

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -16,14 +16,17 @@ const LoginForm = ({ providers }:ILoginForm) => {
       className="bg-white shadow-md rounded p-8 items-center flex flex-col gap-4"
     >
       <h2 className="text-center text-2xl font-bold tracking text-gray-900">Welcome to Degree Door</h2>
-      <button className="p-4 py-2 text-md text-indigo-500 font-semibold rounded-full border
-        border-indigo-500 hover:bg-indigo-500 hover:text-gray-50 hover:border-transparent 
-        duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center gap-4"
-        onClick={() => signIn(discord.id)}
-      >
-        <SiDiscord />
-        <p>Continue with Discord</p>
-      </button>
+      <div key={discord.name}>
+        <button className="p-4 py-2 text-md text-indigo-500 font-semibold rounded-full border
+          border-indigo-500 hover:bg-indigo-500 hover:text-gray-50 hover:border-transparent 
+          duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center gap-4"
+          onClick={() => signIn(discord.id)}
+          type="button"
+        >
+          <SiDiscord />
+          <p>Continue with Discord</p>
+        </button>
+      </div>
     </motion.form>
   );
 }
