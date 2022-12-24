a13a48137bccc2177c3dc428325437b4dfdd1998
Patrick <patrickducusin2@gmail.com>
2022-12-23 19:37:40 -0500

    Create a login form for Degree Door with animation

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import { SiDiscord } from 'react-icons/si'
+import { motion } from 'framer-motion'
+
+const LoginForm = () => {
+
+  return (
+    <motion.form
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="bg-white shadow-md rounded p-8 items-center flex flex-col gap-4"
+    >
+      <h2 className="text-center text-2xl font-bold tracking text-gray-900">Welcome to Degree Door</h2>
+      <button className="p-4 py-2 text-md text-indigo-500 font-semibold rounded-full border
+        border-indigo-500 hover:bg-indigo-500 hover:text-gray-50 hover:border-transparent 
+        duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center gap-4"
+      >
+        <SiDiscord />
+        <p>Coninue with Discord</p>
+      </button>
+    </motion.form>
+  )
+}
+
+export default LoginForm
