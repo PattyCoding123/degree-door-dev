f3f5c12548fb1d70a271396ef35814b543bdbf9e
Patrick <patrickducusin2@gmail.com>
2022-12-25 22:12:51 -0500

    Remove provider props form LoginForm since we only use Discord for now

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,8 @@
 import { SiDiscord } from 'react-icons/si';
 import { signIn } from 'next-auth/react';
 import { motion } from 'framer-motion';
-import { ProviderProps } from 'next-auth';
 
-interface ILoginForm {
-  providers: ProviderProps[];
-}
-
-const LoginForm:React.FC<ILoginForm> = ({ providers }) => {
-  const discord = providers[0];
+const LoginForm:React.FC = () => {
 
   return (
     <motion.form
@@ -18,11 +12,11 @@ const LoginForm:React.FC<ILoginForm> = ({ providers }) => {
       className="bg-white shadow-md rounded p-8 items-center flex flex-col gap-4"
     >
       <h2 className="text-center text-2xl font-bold tracking text-gray-900">Welcome to Degree Door</h2>
-      <div key={discord!.name}>
+      <div>
         <button className="p-4 py-2 text-md text-indigo-500 font-semibold rounded-full border
           border-indigo-500 hover:bg-indigo-500 hover:text-gray-50 hover:border-transparent 
           duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center gap-4"
-          onClick={() => signIn(discord!.id)}
+          onClick={() => signIn("discord")}
           type="button"
         >
           <SiDiscord />
