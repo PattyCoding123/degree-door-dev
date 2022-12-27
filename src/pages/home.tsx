b391925449650fb214dc5dfaac2b51f4cd717b55
Patrick <patrickducusin2@gmail.com>
2022-12-27 16:44:36 -0500

    Set up main and section stylings

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,14 @@
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (<div>Home Page</div>)
+  return (
+    <main className="bg-tokyo-night text-white h-screen w-screen flex items-center justify-center">
+      <section className="max-w-md rounded-lg shadow-xl p-6">
+        <p className="text-tokyo-night">Hello, this is my home page.</p>
+      </section>
+    </main>
+  );
+
 }
+
+export default Home;
