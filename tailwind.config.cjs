ad53e26d377c0357c407b2b4328c359049674817
Patrick <patrickducusin2@gmail.com>
2022-12-27 16:44:11 -0500

    Add tokyo night background and text to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'tokyo-night': '#0a0047',
+      },
+      textColor: {
+        'tokyo-night': '#ffffff',
+      },
+    },
   },
   plugins: [],
 };
