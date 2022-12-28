8e82fe93937c7b52b382ec7374ec9f03fd4ee141
Patrick <patrickducusin2@gmail.com>
2022-12-28 17:41:57 -0500

    Add section and h1 to post page

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -7,8 +7,10 @@ const Post: NextPage = () => {
 
   return (
     <main className="h-screen w-screen bg-gradient-to-t from-blue-800 to-indigo-900">
-      This is my post page.
-      <ForumForm />
+      <section>
+        <h1 className="text-slate-200 text-4xl text-center p-8">Write your Review</h1>
+        <ForumForm />
+      </section>
     </main>
   );
 }
