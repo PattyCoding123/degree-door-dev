5f7fc065586b2ab94c882033b0ff51005454e430
Patrick <patrickducusin2@gmail.com>
2022-12-27 19:05:24 -0500

    Include Course into ForumForm

--- src/components/forms/ForumForm.tsx
+++ src/components/forms/ForumForm.tsx
@@ -1,7 +1,20 @@
+import { useForm } from "react-hook-form";
+
+interface ForumFormData {
+  course: string;
+  pros: string;
+  cons: string;
+}
+
 const ForumForm: React.FC = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<ForumFormData>();
+  const onSubmit = handleSubmit(data => console.log(data));
   return (
-    <form className="w-1/2">
-      MY FORM
+    <form className="w-1/2" onSubmit={onSubmit}>
+      <label>Course</label>
+      <input {...register("course", { required: true, pattern: /^[A-Z]{2}\s[0-9]{4}$/i})}/>
+
+      <label>Pros</label>
     </form>
   )
 }
