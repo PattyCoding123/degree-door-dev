7fbcd33c3ffe6442504f1f8c77f862beb0a6080c
Patrick <patrickducusin2@gmail.com>
2022-12-27 19:09:32 -0500

    Add pros and cons fields inside ForumForm

--- src/components/forms/ForumForm.tsx
+++ src/components/forms/ForumForm.tsx
@@ -9,14 +9,19 @@ interface ForumFormData {
 const ForumForm: React.FC = () => {
   const { register, handleSubmit, formState: { errors } } = useForm<ForumFormData>();
   const onSubmit = handleSubmit(data => console.log(data));
+
   return (
     <form className="w-1/2" onSubmit={onSubmit}>
       <label>Course</label>
       <input {...register("course", { required: true, pattern: /^[A-Z]{2}\s[0-9]{4}$/i})}/>
 
       <label>Pros</label>
+      <input {...register("pros", { required: true })}/>
+
+      <label>Cons</label>
+      <input {...register("cons", { required: true })}/>
     </form>
-  )
+  );
 }
 
 export default ForumForm;
