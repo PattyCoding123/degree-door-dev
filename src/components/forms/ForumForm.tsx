6f02def2201923b7430067ceb5ab85e505366182
Patrick <patrickducusin2@gmail.com>
2022-12-27 20:46:37 -0500

    Add stylings to the ForumForm input fields

--- src/components/forms/ForumForm.tsx
+++ src/components/forms/ForumForm.tsx
@@ -11,15 +11,36 @@ const ForumForm: React.FC = () => {
   const onSubmit = handleSubmit(data => console.log(data));
 
   return (
-    <form className="w-1/2 bg-gradient-to-r from-slate-300 to-slate-500" onSubmit={onSubmit}>
-      <label>Course</label>
-      <input {...register("course", { required: true, pattern: /^[A-Z]{2}\s[0-9]{4}$/i})}/>
-
-      <label>Pros</label>
-      <input {...register("pros", { required: true })}/>
-
-      <label>Cons</label>
-      <input {...register("cons", { required: true })}/>
+    <form onSubmit={onSubmit}>
+      <div className="w-1/2 p-8 bg-slate-200 mx-auto rounded-md shadow-2xl">
+        <div className="mb-6">
+          <label htmlFor="course-id" className="block text-xl font-medium text-gray-700">Course</label>
+          <input 
+            id="course-id-input" 
+            className="block w-56 max-w-fit flex-1 rounded-md border-gray-700 p-4 mt-2
+            sm:text-sm hover:shadow-xl duration-200 outline-none" 
+            {...register("course", { required: true, pattern: /^[A-Z]{2}\s[0-9]{4}$/i})}
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="pros" className="block text-xl font-medium text-gray-700">Pros</label>
+          <textarea
+            id="pros-input"
+            className="block w-full h-32 max-h-fit flex-1 rounded-md border-gray-700 p-4 mt-2
+            sm:text-sm hover:shadow-xl duration-200 outline-none"
+            {...register("pros", { required: true })}
+          />
+        </div>
+        <div>
+          <label htmlFor="cons" className="block text-xl font-medium text-gray-700">Cons</label>
+          <textarea 
+            id="cons-input"
+            className="block w-full h-32 max-h-fit flex-1 rounded-md border-gray-700 p-4 mt-2
+            sm:text-sm hover:shadow-xl duration-200 outline-none"
+            {...register("cons", { required: true })}
+          />
+        </div>
+      </div>
     </form>
   );
 }
