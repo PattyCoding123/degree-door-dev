949203642aee18f94c6ed264e2431b0a7603431d
Patrick <patrickducusin2@gmail.com>
2022-12-29 00:51:24 -0500

    Implement example onSubmiut using createPost

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,15 +1,20 @@
 import { type NextPage } from "next";
 
-import ForumForm from "../components/forms/ForumForm";
+import { trpc } from "../utils/trpc";
+import ForumForm, { ForumFormData } from "../components/forms/ForumForm";
 
 const Post: NextPage = () => {
+  const createPost = trpc.forum.createPost.useMutation();
 
+  const onSubmit = async (data: ForumFormData) => {
+    createPost.mutate(data);
+  }
 
   return (
     <main className="h-screen w-screen bg-gradient-to-t from-blue-800 to-indigo-900">
       <section>
         <h1 className="text-slate-200 text-4xl text-center p-8">Write your Review</h1>
-        <ForumForm />
+        <ForumForm onSubmit={onSubmit}/>
       </section>
     </main>
   );
