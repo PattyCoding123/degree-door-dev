0a5d3a09f6f1740e03042592fbdbd1aba7675f2e
Patrick <patrickducusin2@gmail.com>
2022-12-29 02:10:29 -0500

    Add home page for degree forum

---
+++ src/pages/[degree]/index.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const DegreeHome: NextPage = () => {
+  return (
+    <main>
+      This is the Degree Home page
+    </main>
+  );
+}
+
+export default DegreeHome;
