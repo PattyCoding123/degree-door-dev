d261200cda55a9f90cdadb575c68e6ba77562458
Patrick <patrickducusin2@gmail.com>
2022-12-29 21:46:32 -0500

    Insert the Navbar inside the Degree Forum page

--- src/pages/[degree]/index.tsx
+++ src/pages/[degree]/index.tsx
@@ -1,8 +1,10 @@
 import { type NextPage } from "next";
+import DegreeNavbar from "../../components/DegreeNavbar";
 
 const DegreeHome: NextPage = () => {
   return (
     <main>
+      <DegreeNavbar />
       This is the Degree Home page
     </main>
   );
