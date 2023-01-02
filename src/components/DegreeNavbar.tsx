dfa550b221bc160c0c0e23c4bed8631dba29a67b
Patrick <patrickducusin2@gmail.com>
2023-01-01 21:47:42 -0500

    Change background color of Navbar to ice gradient

--- src/components/DegreeNavbar.tsx
+++ src/components/DegreeNavbar.tsx
@@ -7,7 +7,7 @@ const DegreeNavbar: React.FC = () => {
       <header className="bg-gray-900 py-1">
         <h1 className="font-bold text-white text-center">Computer Science</h1>
       </header>
-      <div className="p-4 flex flex-col md:grid md:grid-cols-3 justify-between items-center gap-8">
+      <div className="p-4 flex flex-col md:grid md:grid-cols-3 justify-between items-center gap-8 bg-gradient-to-b from-rose-100 to-teal-100">
         <div className="col-span-1 navbar-brand cursor-pointer flex">
           <Image src="/degree_door_logo_filled.png" alt="Degree Door Logo" width={24} height={24} />
           <p className="font-bold">egree Door</p>
