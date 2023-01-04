bbfeebdbd85289657bf89d22bb7bdf853ced523a
Patrick <patrickducusin2@gmail.com>
2023-01-03 19:03:39 -0500

    Move Navbar outside main content in reviews page

--- src/pages/[degree]/reviews.tsx
+++ src/pages/[degree]/reviews.tsx
@@ -11,25 +11,26 @@ const DegreeHome: NextPage = () => {
   }
 
   return (
-    <main>
-      <div className="w-screen h-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
-        <DegreeNavbar />
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+      <DegreeNavbar />
+      <main>
         <div 
-          className="h-1/3 mt-8 mx-auto relative items-center justify-center flex 
+          className="h-80 mt-8 mx-auto relative items-center justify-center flex 
           border w-2/3 rounded-xl shadow-2xl bg-gradient-to-b from-rose-100 to-teal-100"
         >
           <section className="absolute flex flex-col gap-2 text-center">
             <h1 className="text-4xl md:text-6xl font-bold">Computer Science</h1>
-            <p className="text-lg md:text-xl">Reviews</p>
+            <p className="text-xl md:text-3xl">Reviews</p>
           </section>
         </div>
         <section className="mt-8">
-          <div className="mt-8 flex flex-col md:flex-row items-center align-middle justify-center gap-16">
+          <div className="mt-8 flex flex-col items-center align-middle justify-center gap-10">
+            <Review course={sample.course} pros={sample.pros} cons={sample.cons}/>
             <Review course={sample.course} pros={sample.pros} cons={sample.cons}/>
           </div>
         </section>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
 
