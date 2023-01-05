9a538a1c331c9d591a83dd07107af1a9b5b83ab7
Patrick <patrickducusin2@gmail.com>
2023-01-04 21:56:48 -0500

    Add toasts for success and error scenarios for creating posts

--- src/pages/[degree]/post.tsx
+++ src/pages/[degree]/post.tsx
@@ -1,13 +1,19 @@
 import { type NextPage } from "next";
+import { Toaster, toast } from "react-hot-toast";
 import { useRouter } from "next/router";
 
 import { trpc } from "../../utils/trpc";
 import ForumForm, { type ForumFormData } from "../../components/forms/ForumForm";
 import DegreeNavbar from "../../components/DegreeNavbar";
 
+
 const Post: NextPage = () => {
   const { degree } = useRouter().query as { degree: string };
-  const createPost = trpc.forum.createPost.useMutation<ForumFormData>();
+  const createPost = trpc.forum.createPost.useMutation<ForumFormData>({
+    onSuccess: () => toast.success("Review successfully created!", { position: "bottom-center", className: "text-xl" }),
+    onError: () => toast.error("There was an error creating the post!", { position: "bottom-center", className: "text-xl" })
+  });
+
   const onSubmit = async (data: ForumFormData) => {
     const review = await createPost.mutateAsync({degreeId: degree, formData: data});
     console.log(review);
@@ -15,6 +21,7 @@ const Post: NextPage = () => {
 
   return (
     <div className="min-h-screen w-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+      <Toaster />
       <DegreeNavbar />
       <main>
         <section>
