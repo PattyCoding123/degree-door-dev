d2f71537d3c8cab5b79a0f345c7ccb38223998e5
Patrick <patrickducusin2@gmail.com>
2023-01-08 00:21:12 -0500

    Include more Review props for degreeId and a handleClick event

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,18 +1,22 @@
 import { IoMdThumbsUp, IoMdThumbsDown } from "react-icons/io";
 import { BsFillTrashFill } from "react-icons/bs";
+import { BaseSyntheticEvent } from "react";
 
 interface ReviewProps {
   course: string;
   pros: string;
   cons: string;
+  reviewId: string;
+  handleClick: (event: BaseSyntheticEvent) => void;
 }
 
-const Review: React.FC<ReviewProps> = ({ course, pros, cons }) => {
+const Review: React.FC<ReviewProps> = ({ course, pros, cons, reviewId, handleClick }) => {
+
   return (
     <article className="w-2/3 p-4 border bg-gradient-to-b from-rose-100 to-teal-100 rounded-xl shadow-2xl flex flex-col gap-2">
       <div className="flex items-center justify-between">
         <h1 className="text-center text-lg font-bold">{course}</h1>
-        <BsFillTrashFill className="text-lg"/>
+        <BsFillTrashFill id={reviewId} className="text-lg" onClick={handleClick}/>
       </div>
       <section className="flex">
         <div className="bg-white border-2 border-green-700 w-16 p-2 flex justify-center items-center rounded">
