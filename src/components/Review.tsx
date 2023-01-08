ce9ff10f3f95892bfd5aeac3cfd0db94316f296c
Patrick <patrickducusin2@gmail.com>
2023-01-08 02:27:58 -0500

    conditionally render trash icon if user created review

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,22 +1,24 @@
 import { IoMdThumbsUp, IoMdThumbsDown } from "react-icons/io";
 import { BsFillTrashFill } from "react-icons/bs";
 import { BaseSyntheticEvent } from "react";
+import { useSession } from "next-auth/react";
 
 interface ReviewProps {
   course: string;
   pros: string;
   cons: string;
   reviewId: string;
+  userId: string;
   handleClick: (event: BaseSyntheticEvent) => void;
 }
 
-const Review: React.FC<ReviewProps> = ({ course, pros, cons, reviewId, handleClick }) => {
-
+const Review: React.FC<ReviewProps> = ({ course, pros, cons, reviewId, userId, handleClick }) => {
+  const { data: sessionData } = useSession();
   return (
-    <article className="w-2/3 p-4 border bg-gradient-to-b from-rose-100 to-teal-100 rounded-xl shadow-2xl flex flex-col gap-2">
+    <article className="w-2/3 p-4 border bg-gradient-to-b from-rose-100 to-teal-100 rounded-xl shadow-2xl flex flex-col gap-4">
       <div className="flex items-center justify-between">
         <h1 className="text-center text-lg font-bold">{course}</h1>
-        <BsFillTrashFill id={reviewId} className="text-lg" onClick={handleClick}/>
+        { sessionData?.user?.id === userId && <BsFillTrashFill id={reviewId} className="text-lg cursor-pointer" onClick={handleClick}/>}
       </div>
       <section className="flex">
         <div className="bg-white border-2 border-green-700 w-16 p-2 flex justify-center items-center rounded">
