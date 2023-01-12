decaa8e7a16a8fc6956a9b51ac3766f87147a6b7
Patrick <patrickducusin2@gmail.com>
2023-01-11 23:58:12 -0500

    Make DegreeNavbar take new props for degreeName and degreeId

--- src/components/DegreeNavbar.tsx
+++ src/components/DegreeNavbar.tsx
@@ -1,20 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import clsx from "clsx";
-import { useRouter } from "next/router";
 import { BsFillGearFill } from "react-icons/bs";
 
 interface DegreeNavbarProps {
   active: string;
+  degreeName?: string;
+  degreeId?: string;
 }
 
-const DegreeNavbar: React.FC<DegreeNavbarProps> = ({ active }) => {
-  const { degree } = useRouter().query as { degree: string };
-
+const DegreeNavbar: React.FC<DegreeNavbarProps> = ({ active, degreeName, degreeId }) => {
   return (
     <nav className="max-w-screen m-auto flex flex-col shadow-lg">
       <header className="bg-gray-900 py-1">
-        <h1 className="font-bold text-white text-center">Computer Science</h1>
+        <h1 className="font-bold text-white text-center">{degreeName}</h1>
       </header>
       <div className="p-4 flex flex-col md:grid md:grid-cols-3 justify-between items-center gap-8 bg-gradient-to-b from-rose-100 to-teal-100">
         <Link href="/">
@@ -25,17 +24,17 @@ const DegreeNavbar: React.FC<DegreeNavbarProps> = ({ active }) => {
         </Link>
         <ul className="flex flex-col md:flex-row md:col-span-1 items-center justify-center gap-10 md:gap-4">
           <li>
-            <Link href={`/${degree}`}>
+            <Link href={`/${degreeId}`}>
               <p className={clsx("font-bold hover:opacity-50", {"text-indigo-500": active === "overview"})}>OVERVIEW</p>
             </Link>
           </li>
           <li>
-            <Link href={`/${degree}/reviews`}>
+            <Link href={`/${degreeId}/reviews`}>
               <p className={clsx("font-bold hover:opacity-50", {"text-indigo-500": active === "reviews"})}>REVIEWS</p>
             </Link>
           </li>
           <li>
-            <Link href={`/${degree}/post`}>
+            <Link href={`/${degreeId}/post`}>
               <p className={clsx("font-bold hover:opacity-50", {"text-indigo-500": active === "post"})}>POST A REVIEW</p>
             </Link>
           </li>
