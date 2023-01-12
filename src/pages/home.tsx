ef6c6e2219a450dcc2489387c4d7ea0f5b257d22
Patrick <patrickducusin2@gmail.com>
2023-01-11 23:00:26 -0500

    Change background gradient for home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import HeroBanner from "../components/HeroBanner";
 
 const Home: NextPage = () => {
   return (
-    <main className="bg-gradient-to-b from-[#00076f] to-[#44008b] text-white h-screen w-screen">
+    <main className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 text-white h-screen w-screen">
       <HeroBanner />
       <section className="max-w-md rounded-lg shadow-xl p-6 mx-auto">
         <p className="text-tokyo-night">Hello, this is my home page.</p>
