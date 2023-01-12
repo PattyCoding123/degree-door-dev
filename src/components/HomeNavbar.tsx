7bc54af479d45e1e552282928b2d843182e2af8d
Patrick <patrickducusin2@gmail.com>
2023-01-12 00:30:46 -0500

    Finish creating base of HomeNavbar

--- src/components/HomeNavbar.tsx
+++ src/components/HomeNavbar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BsFillGearFill } from "react-icons/bs";
 
 const HomeNavbar: React.FC = () => {
   return (
     <nav className="max-w-screen bg-gray-900">
+      <div className="px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-8">
+        <Link href="/">
+          <div className="col-span-1 navbar-brand cursor-pointer flex">
+            <Image src="/degree_door_logo.png" alt="Degree Door Logo" width={24} height={24} />
+            <p className="font-bold">egree Door</p>
+          </div>
+        </Link>
+        <BsFillGearFill className="text-lg text-white" />
+      </div>
     </nav>
   );
 }
