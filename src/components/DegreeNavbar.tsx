884126317bade23e0040909771789062b6093a3b
Patrick <patrickducusin2@gmail.com>
2023-01-12 00:32:17 -0500

    Add px and py into div for Links in DegreeNavbar

--- src/components/DegreeNavbar.tsx
+++ src/components/DegreeNavbar.tsx
@@ -15,7 +15,7 @@ const DegreeNavbar: React.FC<DegreeNavbarProps> = ({ active, degreeName, degreeI
       <header className="bg-gray-900 py-1">
         <h1 className="font-bold text-white text-center">{degreeName}</h1>
       </header>
-      <div className="p-4 flex flex-col md:grid md:grid-cols-3 justify-between items-center gap-8 bg-gradient-to-b from-rose-100 to-teal-100">
+      <div className="px-6 py-4 flex flex-col md:grid md:grid-cols-3 justify-between items-center gap-8 bg-gradient-to-b from-rose-100 to-teal-100">
         <Link href="/">
           <div className="col-span-1 navbar-brand cursor-pointer flex">
             <Image src="/degree_door_logo_filled.png" alt="Degree Door Logo" width={24} height={24} />
