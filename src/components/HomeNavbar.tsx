53a00067070eeda4044fdfaea6f48544618a5401
PattyCoding123 <patrickducusin2@gmail.com>
2023-01-16 15:47:15 -0500

    Allow HomeNavbar to take in children props

--- src/components/HomeNavbar.tsx
+++ src/components/HomeNavbar.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 import { BsFillGearFill } from "react-icons/bs";
 
-const HomeNavbar: React.FC = () => {
+interface HomeNavbarProps {
+  children?: React.ReactNode
+}
+
+const HomeNavbar: React.FC<HomeNavbarProps> = ({ children }) => {
   return (
     <nav className="max-w-screen bg-gray-900">
       <div className="px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-8">
@@ -12,6 +17,7 @@ const HomeNavbar: React.FC = () => {
             <p className="font-bold">egree Door</p>
           </div>
         </Link>
+        {children}
         <BsFillGearFill className="text-lg text-white" />
       </div>
     </nav>
