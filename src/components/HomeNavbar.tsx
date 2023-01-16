064d625f5de02a8d3302248ac6b60e794283e2b8
PattyCoding123 <patrickducusin2@gmail.com>
2023-01-16 17:13:03 -0500

    Change styling of HomeNavbar to create equal size coloumns

--- src/components/HomeNavbar.tsx
+++ src/components/HomeNavbar.tsx
@@ -10,15 +10,19 @@ interface HomeNavbarProps {
 const HomeNavbar: React.FC<HomeNavbarProps> = ({ children }) => {
   return (
     <nav className="max-w-screen bg-gray-900">
-      <div className="px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-8">
+      <div className="px-6 py-4 flex flex-col items-center gap-8 md:grid md:grid-cols-3 w-full">
         <Link href="/">
           <div className="col-span-1 navbar-brand cursor-pointer flex">
             <Image src="/degree_door_logo.png" alt="Degree Door Logo" width={24} height={24} />
             <p className="font-bold">egree Door</p>
           </div>
         </Link>
-        {children}
-        <BsFillGearFill className="text-lg text-white" />
+        <div className="col-span-1 justify-self-center">
+          {children}
+        </div>
+        <div className="col-span-1 items-center justify-self-end">
+          <BsFillGearFill className="text-lg text-white" />
+        </div>
       </div>
     </nav>
   );
