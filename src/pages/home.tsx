42a9d9f00d9b1f717a8a8f5752958ec9ceb69cc4
PattyCoding123 <patrickducusin2@gmail.com>
2023-01-16 17:11:31 -0500

    Include Searchbar inside HomeNavbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,11 +2,14 @@ import { type NextPage } from "next";
 
 import HeroBanner from "../components/HeroBanner";
 import HomeNavbar from "../components/HomeNavbar";
+import Searchbar from "../components/Searchbar";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 text-white h-screen w-screen">
-      <HomeNavbar />
+      <HomeNavbar>
+        <Searchbar />
+      </HomeNavbar>
       <main>
         <HeroBanner />
         <section className="max-w-md rounded-lg shadow-xl p-6 mx-auto">
