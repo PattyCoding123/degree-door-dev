14bb9bb1c88ab0fef0eaeb89ad2f6e62577f8a11
PattyCoding123 <patrickducusin2@gmail.com>
2023-01-23 15:30:35 -0500

    Remove special prop as no functionality

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,11 +1,5 @@
 
-
-interface ButtonProps 
-extends React.ComponentPropsWithoutRef<"button"> {
-  specialProp?: string;
-};
-
-export function Button(props: ButtonProps) {
+export function Button(props: React.ComponentPropsWithoutRef<"button">) {
   const activeButton = "inline-block px-6 py-2.5 bg-rose-400 text-white font-medium cursor-pointer " +
   "text-sm leading-tight uppercase rounded shadow-md hover:opacity-80 hover:shadow-lg hover:scale-90 "+
   "duration-200 ease-in-out";
@@ -13,7 +7,7 @@ export function Button(props: ButtonProps) {
   const disabledButton = "inline-block px-6 py-2.5 bg-rose-400 text-white font-medium " +
   "text-sm leading-tight uppercase rounded shadow-md opacity-50";
 
-  const { specialProp, className, disabled, ...rest } = props;
+  const { className, disabled, ...rest } = props;
   return <button
     className={(disabled ? disabledButton : activeButton) + " " + className}
     disabled={disabled}
