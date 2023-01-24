0672d9561da31c466747af2e001ccffa4d48bc75
PattyCoding123 <patrickducusin2@gmail.com>
2023-01-24 12:31:43 -0500

    Remove unused imports and router form post page

--- src/pages/[degree]/post.tsx
+++ src/pages/[degree]/post.tsx
@@ -1,24 +1,10 @@
 import { type NextPage } from "next";
-import { useRouter } from "next/router";
-import { Toaster, toast } from "react-hot-toast";
-import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
-import { trpc } from "../../utils/trpc";
-import ForumForm, { type ForumFormData } from "../../components/forms/ForumForm";
+import ForumForm from "../../components/forms/ForumForm";
 import DegreeNavbar from "../../components/DegreeNavbar";
 
-
 const Post: NextPage = () => {
-  const router = useRouter();
-  const { degree } = router.query as { degree: string };
-
-  const degreeQuery = trpc.forum.getDegreeInfo.useQuery({ degreeId: degree }, { enabled: false });
-
-  useEffect(() => {
-    if (!router.isReady) return;
-    degreeQuery.refetch();
-  }, [router.isReady])
-
   return (
     <div className="min-h-screen w-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
       <Toaster />
