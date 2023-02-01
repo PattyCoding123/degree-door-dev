8486fa09d369b754df1998b7acafac7f8d3b9159
Patrick <patrickducusin2@gmail.com>
2023-01-31 23:38:21 -0500

    Utilize ensure helper in degreeQuery and reformat

--- src/pages/[degree]/index.tsx
+++ src/pages/[degree]/index.tsx
@@ -3,57 +3,79 @@ import { useRouter } from "next/router";
 
 import { trpc } from "../../utils/trpc";
 import DegreeNavbar from "../../components/DegreeNavbar";
+import ensure from "../../utils/ensure";
 
 const DegreeHome: NextPage = () => {
   const { degree } = useRouter().query as { degree: string | undefined };
 
   // Dependent query, will not run unless degree is definied: !!variable => boolean
-  const degreeQuery = trpc.forum.getDegreeInfo.useQuery({ degreeId: degree! }, { enabled: !!degree });
+  const degreeQuery = trpc.forum.getDegreeInfo.useQuery(
+    { degreeId: ensure(degree) },
+    { enabled: !!degree }
+  );
 
   return (
     <div className="max-w-screen min-h-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
-      <DegreeNavbar 
-        active="overview" 
+      <DegreeNavbar
+        active="overview"
         degreeName={degreeQuery.data?.name}
         degreeId={degreeQuery.data?.id}
       />
       <main className="p-8">
-        <div 
-          className="h-80 mt-8 mx-auto relative border w-2/3 
-          rounded-xl shadow-2xl bg-gradient-to-b from-rose-100 to-teal-100"
+        <div
+          className="relative mx-auto mt-8 h-80 w-2/3 rounded-xl 
+          border bg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl"
         >
           <section className="absolute inset-0 flex flex-col justify-center gap-2 text-center">
-            <h1 className="text-4xl md:text-6xl font-bold">{degreeQuery.data?.name}</h1>
-            <p className="text-lg md:text-xl">Apart of the College of Engineering at Wayne State University</p>
+            <h1 className="text-4xl font-bold md:text-6xl">
+              {degreeQuery.data?.name}
+            </h1>
+            <p className="text-lg md:text-xl">
+              Apart of the College of Engineering at Wayne State University
+            </p>
           </section>
         </div>
         <section className="mt-8">
-          <h1 className="text-white text-xl text-center font-bold">More Info...</h1>
-          <div className="mt-8 flex flex-col md:flex-row items-center align-middle justify-center gap-16">
-            <article className="w-80 h-80 p-4 border bg-gradient-to-b from-rose-100 to-teal-100 rounded-xl shadow-2xl">
+          <h1 className="text-center text-xl font-bold text-white">
+            More Info...
+          </h1>
+          <div className="mt-8 flex flex-col items-center justify-center gap-16 align-middle md:flex-row">
+            <article className="h-80 w-80 rounded-xl border bg-gradient-to-b from-rose-100 to-teal-100 p-4 shadow-2xl">
               <div className="flex flex-col gap-2">
-                <h2 className="text-center font-bold">Information about the University</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quasi suscipit voluptatem natus!
-                  Eum, deleniti fugit. Illo veniam laborum distinctio placeat fugiat temporibus molestiae, quis 
-                  voluptates soluta debitis cupiditate neque!
+                <h2 className="text-center font-bold">
+                  Information about the University
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolore quasi suscipit voluptatem natus! Eum, deleniti fugit.
+                  Illo veniam laborum distinctio placeat fugiat temporibus
+                  molestiae, quis voluptates soluta debitis cupiditate neque!
                 </p>
               </div>
             </article>
-            <article className="w-80 h-80 p-4 border bg-gradient-to-b from-rose-100 to-teal-100 rounded-xl shadow-2xl">
+            <article className="h-80 w-80 rounded-xl border bg-gradient-to-b from-rose-100 to-teal-100 p-4 shadow-2xl">
               <div className="flex flex-col gap-2">
-                <h2 className="text-center font-bold">Information about the University</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quasi suscipit voluptatem natus!
-                  Eum, deleniti fugit. Illo veniam laborum distinctio placeat fugiat temporibus molestiae, quis 
-                  voluptates soluta debitis cupiditate neque!
+                <h2 className="text-center font-bold">
+                  Information about the University
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolore quasi suscipit voluptatem natus! Eum, deleniti fugit.
+                  Illo veniam laborum distinctio placeat fugiat temporibus
+                  molestiae, quis voluptates soluta debitis cupiditate neque!
                 </p>
               </div>
             </article>
-            <article className="w-80 h-80 p-4 border bg-gradient-to-b from-rose-100 to-teal-100 rounded-xl shadow-2xl">
+            <article className="h-80 w-80 rounded-xl border bg-gradient-to-b from-rose-100 to-teal-100 p-4 shadow-2xl">
               <div className="flex flex-col gap-2">
-                <h2 className="text-center font-bold">Information about the University</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quasi suscipit voluptatem natus!
-                  Eum, deleniti fugit. Illo veniam laborum distinctio placeat fugiat temporibus molestiae, quis 
-                  voluptates soluta debitis cupiditate neque!
+                <h2 className="text-center font-bold">
+                  Information about the University
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolore quasi suscipit voluptatem natus! Eum, deleniti fugit.
+                  Illo veniam laborum distinctio placeat fugiat temporibus
+                  molestiae, quis voluptates soluta debitis cupiditate neque!
                 </p>
               </div>
             </article>
@@ -62,6 +84,6 @@ const DegreeHome: NextPage = () => {
       </main>
     </div>
   );
-}
+};
 
 export default DegreeHome;
