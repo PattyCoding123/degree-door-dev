f2ec08c41609dec063e86305d74728632817d27d
Patrick <patrickducusin2@gmail.com>
2023-02-05 16:24:53 -0500

    Move authshowcase to profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,8 @@
 import { type NextPage } from "next";
+import { useSession, signOut, signIn } from "next-auth/react";
 import Image from "next/image";
 
+import { trpc } from "../utils/trpc";
 import HomeNavbar from "../components/HomeNavbar";
 
 const Profile: NextPage = () => {
@@ -89,7 +91,32 @@ const Profile: NextPage = () => {
             </form>
           </div>
         </section>
+        <AuthShowcase />
       </main>
+    </div>
+  );
+};
+
+const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => signOut() : () => signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
     </div>
   );
 };
