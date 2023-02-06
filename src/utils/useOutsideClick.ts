863ce7fb4e96955c99925ea58f6657ed77ad82f9
Patrick <patrickducusin2@gmail.com>
2023-02-05 23:23:14 -0500

    Add more documentation to useOutsideClick hook

--- src/utils/useOutsideClick.ts
+++ src/utils/useOutsideClick.ts
@@ -1,11 +1,19 @@
 import { useEffect, useRef } from "react";
 
 /*
- * useClickOutside hook returns a ref which you can attach to a DOM element in your component. When someone clicks or touches outside of the element, the callback function will be triggered.
+  * useClickOutside hook returns a ref which you can attach to a DOM element in your component. 
+  * When someone clicks or touches outside of the element, the callback function will be triggered.
+
  */
 const useClickOutside = (callback: () => void) => {
   const ref = useRef<HTMLDivElement>(null);
 
+  /* 
+    We check !ref.current.contains(event.target as Node) because we only want
+    the callback to trigger when the click happens outside the component.
+
+    event.target is casted as Node to limit down target types to DOM elements.
+  */
   const handleClick = (event: MouseEvent | TouchEvent) => {
     if (ref.current && !ref.current.contains(event.target as Node)) {
       callback();
