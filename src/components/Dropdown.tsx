3928fed3842b79c518be380e9a897665fb4da3d1
Patrick <patrickducusin2@gmail.com>
2023-02-05 20:28:47 -0500

    Utilize framer motion to animate dropdown items

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BsFillGearFill } from "react-icons/bs";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 const Dropdown: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
 
@@ -10,30 +10,47 @@ const Dropdown: React.FC = () => {
         <BsFillGearFill
           onClick={() => setIsVisible(!isVisible)}
           className="align-middle text-lg text-white duration-200 hover:scale-90 hover:cursor-pointer"
+          aria-haspopup="true"
         />
         {isVisible && (
-          <div className="absolute right-0 z-10 mt-4 w-56 origin-top-right rounded-md border border-gray-100 bg-white shadow-lg transition-opacity">
-            <div className="p-2">
-              <a
-                href="#"
-                className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700"
-              >
-                ReactJS Dropdown 1
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700"
-              >
-                ReactJS Dropdown 2
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700"
-              >
-                ReactJS Dropdown 3
-              </a>
-            </div>
-          </div>
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.95 }}
+              className="from absolute right-0 z-10 mt-4 w-56 origin-top-right rounded-md border border-gray-100 bg-white shadow-lg"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+            >
+              <div className="p-2" role="none">
+                <a
+                  href="#"
+                  className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700"
+                  role="menuitem"
+                  id="menu-item-0"
+                >
+                  ReactJS Dropdown 1
+                </a>
+                <a
+                  href="#"
+                  className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700"
+                  role="menuitem"
+                  id="menu-item-1"
+                >
+                  ReactJS Dropdown 2
+                </a>
+                <a
+                  href="#"
+                  className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700"
+                  role="menuitem"
+                  id="menu-item-2"
+                >
+                  ReactJS Dropdown 3
+                </a>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         )}
       </div>
     </div>
