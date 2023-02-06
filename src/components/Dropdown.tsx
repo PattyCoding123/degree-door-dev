d6bcb497798cd24e402cab61a5739f7aa7219857
Patrick <patrickducusin2@gmail.com>
2023-02-05 22:02:42 -0500

    Make hover bg color darker

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -32,7 +32,7 @@ const Dropdown: React.FC = () => {
                 {sessionData ? (
                   <Link
                     href="/profile"
-                    className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700"
+                    className="block rounded-lg px-4 py-2 text-sm text-gray-600 hover:bg-gray-200 hover:text-gray-700"
                     role="menuitem"
                     id="menu-item-0"
                   >
