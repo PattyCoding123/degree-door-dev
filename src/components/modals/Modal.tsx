b910aa807c6fa7718b63718d2d45c33d03adacec
PattyCoding123 <patrickducusin2@gmail.com>
2023-02-08 18:02:12 -0500

    Create a modal component to use for modal-based components

---
+++ src/components/modals/Modal.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef, type ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.querySelector("#modal-root") as HTMLElement;
+
+type ModalProps = {
+  children: ReactNode;
+};
+
+const Modal = ({ children }: ModalProps) => {
+  // create div element only once using ref
+  const elRef = useRef<HTMLDivElement | null>(null);
+  if (!elRef.current) elRef.current = document.createElement("div");
+
+  useEffect(() => {
+    const el = elRef.current!; // non-null assertion because it will never be null
+    modalRoot.appendChild(el);
+    return () => {
+      modalRoot.removeChild(el);
+    };
+  }, []);
+
+  return createPortal(
+    <div
+      className="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+        {children}
+      </div>
+    </div>,
+    elRef.current
+  );
+};
+
+export default Modal;
