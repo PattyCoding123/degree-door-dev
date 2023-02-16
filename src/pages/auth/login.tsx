47c9a328fb009b5309ec7bec2398894f61dd155c
Patrick <patrickducusin2@gmail.com>
2023-02-15 22:45:14 -0500

    Change import name to specify that only types are allowed

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,35 +1,32 @@
-import { GetServerSideProps, type NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 
 import { getServerAuthSession } from "../../server/common/get-server-auth-session";
 import LoginForm from "../../components/forms/LoginForm";
 
 const Login: NextPage = () => {
   return (
-    <main
-      className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 flex items-center justify-center h-screen flex-col"
-    >
+    <main className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
       <LoginForm />
     </main>
-  )
-}
+  );
+};
 
 export default Login;
 
-export const getServerSideProps: GetServerSideProps = async (context) => { 
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const sessionData = await getServerAuthSession(context); // Get Session
 
   // Redirect to home page if we are already logged in
-  if(sessionData) {
+  if (sessionData) {
     return {
       redirect: {
-        destination: '/',
-        permanent: false
-      }
-    }
+        destination: "/",
+        permanent: false,
+      },
+    };
   }
 
   return {
-    props: {}
+    props: {},
   };
-}
-
+};
