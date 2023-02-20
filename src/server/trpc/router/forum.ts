46cd75b184179f2b48bdec2ab96b74743f708c9c
PattyCoding123 <patrickducusin2@gmail.com>
2023-02-20 12:48:18 -0500

    Create a forum router procedure to allow users to favorite/unfavorite degrees

--- src/server/trpc/router/forum.ts
+++ src/server/trpc/router/forum.ts
@@ -92,4 +92,29 @@ export const forumRouter = router({
 
       return favoriteDegree;
     }),
+  alterFavorite: protectedProcedure
+    .input(z.object({ degreeId: z.string(), isFavorite: z.boolean() }))
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      const prismaDb = ctx.prisma;
+
+      let degree;
+
+      if (input.isFavorite) {
+        degree = await prismaDb.favorites.delete({
+          where: {
+            userId_degreeId: { userId: userId, degreeId: input.degreeId },
+          },
+        });
+      } else {
+        degree = await prismaDb.favorites.create({
+          data: {
+            userId: userId,
+            degreeId: input.degreeId,
+          },
+        });
+      }
+
+      return degree;
+    }),
 });
