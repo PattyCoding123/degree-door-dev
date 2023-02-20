0e6432a3ac8cedd8f5dcbc70d3f488c67831afe4
PattyCoding123 <patrickducusin2@gmail.com>
2023-02-20 12:10:04 -0500

    Add in query for favorite degree check for Favorite component

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -1,13 +1,25 @@
 import { AiOutlineStar } from "react-icons/ai";
 import { useSession } from "next-auth/react";
+import clsx from "clsx";
 
 import { trpc } from "../utils/trpc";
 
-const Favorite = () => {
+interface FavoriteProps {
+  degreeId: string;
+}
+const Favorite: React.FC<FavoriteProps> = ({ degreeId }) => {
   const { data: sessionData } = useSession();
+  const favoriteQuery = trpc.forum.checkIfFavorite.useQuery(
+    {
+      degreeId: degreeId,
+    },
+    {
+      enabled: sessionData?.user !== undefined,
+    }
+  );
 
   if (sessionData?.user !== undefined) {
-    return <AiOutlineStar />;
+    return <AiOutlineStar className={favoriteQuery ? "bg-yellow-500" : ""} />;
   }
   return (
     <button disabled={true}>
