82b8ee915d39fd5e0031a2422959855061b09ff5
PattyCoding123 <patrickducusin2@gmail.com>
2023-02-25 15:14:55 -0500

    Add in Head data for the home page of Degree Door

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,11 @@ import Carousel from "../components/Carousel";
 const Home: NextPage = () => {
   return (
     <>
-      <Head></Head>
+      <Head>
+        <title>Degree Door</title>
+        <meta name="description" content="The Degree Door home page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="h-screen w-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 text-white">
         <HomeNavbar />
         <main>
