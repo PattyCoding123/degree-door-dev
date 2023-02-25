df9fbe5c7549534546f485a2e0776242ff477fc7
PattyCoding123 <patrickducusin2@gmail.com>
2023-02-25 15:07:02 -0500

    Add parentheses around tuple parameter

--- src/utils/usePrevious.ts
+++ src/utils/usePrevious.ts
@@ -4,8 +4,8 @@ export default function usePrevious<T>(state: T): T | undefined {
   const [tuple, setTuple] = useState([undefined, state]);
 
   if (tuple[1] !== state) {
-    setTuple(tuple => [tuple[1], state]); // [prevState, currentState]
+    setTuple((tuple) => [tuple[1], state]); // [prevState, currentState]
   }
-  
+
   return tuple[0];
 }
