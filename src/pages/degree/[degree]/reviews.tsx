7c3ad3838867e4c98d6d5a8cb85ea09029a2f6f6
Patrick <patrickducusin2@gmail.com>
2023-03-06 22:44:02 -0500

    Render Head info but conditionally render everything under the first div

--- src/pages/degree/[degree]/reviews.tsx
+++ src/pages/degree/[degree]/reviews.tsx
@@ -46,51 +46,51 @@ const ReviewsPage: NextPage = () => {
       }),
   });
 
-  if (degreeQuery.isSuccess) {
-    return (
-      <>
-        <Head>
-          <title>Degree Door: {degreeQuery.data.name}</title>
-          <meta name="description" content="Degree reviews page" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className="max-w-screen min-h-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
-          <Toaster />
-          <DegreeNavbar
-            active="reviews"
-            degreeName={degreeQuery.data.name}
-            degreeId={degreeQuery.data.id}
-          />
-          <main className="flex flex-col">
-            <div
-              className="relative mx-auto mt-8 h-80 w-2/3 rounded-xl 
-            border bg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl"
-            >
-              <section className="absolute inset-0 flex flex-col justify-center gap-2 text-center">
-                <h1 className="text-4xl font-bold md:text-6xl">
-                  {degreeQuery.data.name}
-                </h1>
-                <p className="text-xl md:text-3xl">Reviews</p>
+  return (
+    <>
+      <Head>
+        <title>Degree Door Reviews</title>
+        <meta name="description" content="Degree reviews page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="max-w-screen min-h-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+        {degreeQuery.isSuccess && (
+          <>
+            <Toaster />
+            <DegreeNavbar
+              active="reviews"
+              degreeName={degreeQuery.data.name}
+              degreeId={degreeQuery.data.id}
+            />
+            <main className="flex flex-col">
+              <div
+                className="relative mx-auto mt-8 h-80 w-2/3 rounded-xl 
+                border bg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl"
+              >
+                <section className="absolute inset-0 flex flex-col justify-center gap-2 text-center">
+                  <h1 className="text-4xl font-bold md:text-6xl">
+                    {degreeQuery.data.name}
+                  </h1>
+                  <p className="text-xl md:text-3xl">Reviews</p>
+                </section>
+              </div>
+              <section className="my-8 flex flex-col items-center justify-center gap-8 align-middle">
+                {reviewsQuery.data?.map((review) => (
+                  <Review
+                    key={review.id}
+                    reviewPost={review}
+                    handleClick={async (reviewId: string) => {
+                      await deleteReview.mutateAsync({ reviewId: reviewId });
+                    }}
+                  />
+                ))}
               </section>
-            </div>
-            <section className="my-8 flex flex-col items-center justify-center gap-8 align-middle">
-              {reviewsQuery.data?.map((review) => (
-                <Review
-                  key={review.id}
-                  reviewPost={review}
-                  handleClick={async (reviewId: string) => {
-                    await deleteReview.mutateAsync({ reviewId: reviewId });
-                  }}
-                />
-              ))}
-            </section>
-          </main>
-        </div>
-      </>
-    );
-  }
-
-  return null;
+            </main>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ReviewsPage;
