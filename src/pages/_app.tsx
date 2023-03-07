438f69b3afe6bbab4bb3685b1e404e129ac61d9f
Patrick <patrickducusin2@gmail.com>
2023-03-06 23:36:15 -0500

    Add page transitions globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 import { trpc } from "../utils/trpc";
 
@@ -10,11 +12,38 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-      <div id="modal-root" />
-    </SessionProvider>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 0.75,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          animateState: {
+            opacity: 1,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          exitState: {
+            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+          },
+        }}
+        className="base-page-size"
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+          <div id="modal-root" />
+        </SessionProvider>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
