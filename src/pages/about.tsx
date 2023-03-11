7c7096c90a7750d5fe7eb96abf957fcc79d819e8
Patrick <patrickducusin2@gmail.com>
2023-03-10 23:19:21 -0500

    Add content div and Footer into about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Footer from "../components/Footer";
 
 const About: NextPage = () => {
   return (
@@ -9,6 +10,12 @@ const About: NextPage = () => {
         <meta name="description" content="The Degree Door About page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div
+        className="min-w-screen relative min-h-screen bg-gradient-to-r
+       from-rose-400 via-fuchsia-500 to-indigo-500 pb-footer-fit"
+      >
+        <Footer />
+      </div>
     </>
   );
 };
