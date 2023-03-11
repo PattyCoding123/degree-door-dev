bb5daf677c5ad3a1d759c4a057351f7593356fdf
Patrick <patrickducusin2@gmail.com>
2023-03-11 17:46:52 -0500

    Add flex and flex-col to content container in Layout

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -18,8 +18,10 @@ const Layout: React.FC<LayoutProps> = ({ title, description, children }) => {
       </Head>
       <HomeNavbar />
       <div
-        className="min-w-screen relative min-h-screen bg-gradient-to-r
-     from-rose-400 via-fuchsia-500 to-indigo-500 pb-footer-fit-small text-white md:pb-footer-fit"
+        id="content-container"
+        className="min-w-screen relative flex min-h-screen
+        flex-col bg-gradient-to-r from-rose-400 via-fuchsia-500 
+        to-indigo-500 pb-footer-fit-small md:pb-footer-fit"
       >
         {children}
         <Footer />
