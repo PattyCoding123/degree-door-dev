a2f5e88dcd07bc4c425ca0c5e794b75b54333165
Patrick <patrickducusin2@gmail.com>
2023-03-11 18:03:30 -0500

    Make profile page use Layout

--- src/pages/profile/[userId].tsx
+++ src/pages/profile/[userId].tsx
@@ -1,61 +1,50 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { useSession, signOut, signIn } from "next-auth/react";
 import Image from "next/image";
-import Head from "next/head";
 
 import { trpc } from "../../utils/trpc";
 import { getServerAuthSession } from "../../server/common/get-server-auth-session";
-import HomeNavbar from "../../components/navigation/HomeNavbar";
-import Footer from "../../components/Footer";
+import Layout from "../../components/layouts/Layout";
 import ProfileDisplay from "../../components/forms/ProfileDisplay";
 
 const Profile: NextPage = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <>
-      <Head>
-        <title>Degree Door Profile</title>
-        <meta name="description" content="The Degree Door profile page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <HomeNavbar />
-      <div
-        className="min-w-screen relative min-h-screen
-        bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 pb-footer-fit-small md:pb-footer-fit"
-      >
-        <main className="p-8">
-          <section
-            className="mx-auto flex w-1/2 flex-col items-center justify-center
+    <Layout
+      title="Degree Door Profile"
+      description="The Degree Door Profile Page"
+    >
+      <main className="p-8">
+        <section
+          className="mx-auto flex w-1/2 flex-col items-center justify-center
           rounded-md bg-gradient-to-b from-rose-100 to-teal-100 p-8"
-          >
-            <div className="flex flex-col items-center justify-center">
-              <Image
-                className="mb-3 rounded-full shadow-lg"
-                src={
-                  sessionData?.user?.image
-                    ? `${sessionData.user?.image}`
-                    : "/avatar.png"
-                }
-                alt="profile avatar"
-                width={96}
-                height={96}
-                priority={true}
-              />
-              <h1 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                First Name Last Name
-              </h1>
-            </div>
-            <ProfileDisplay
-              displayName={sessionData?.user?.name}
-              email={sessionData?.user?.email}
+        >
+          <div className="flex flex-col items-center justify-center">
+            <Image
+              className="mb-3 rounded-full shadow-lg"
+              src={
+                sessionData?.user?.image
+                  ? `${sessionData.user?.image}`
+                  : "/avatar.png"
+              }
+              alt="profile avatar"
+              width={96}
+              height={96}
+              priority={true}
             />
-          </section>
-          <AuthShowcase />
-        </main>
-        <Footer />
-      </div>
-    </>
+            <h1 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+              First Name Last Name
+            </h1>
+          </div>
+          <ProfileDisplay
+            displayName={sessionData?.user?.name}
+            email={sessionData?.user?.email}
+          />
+        </section>
+        <AuthShowcase />
+      </main>
+    </Layout>
   );
 };
 
