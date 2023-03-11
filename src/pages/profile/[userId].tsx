efe00b922a103e1ddbf025382464bb7426787ba3
Patrick <patrickducusin2@gmail.com>
2023-03-11 17:12:45 -0500

    Add ProfileDisplay component to profile page

--- src/pages/profile/[userId].tsx
+++ src/pages/profile/[userId].tsx
@@ -7,6 +7,7 @@ import { trpc } from "../../utils/trpc";
 import { getServerAuthSession } from "../../server/common/get-server-auth-session";
 import HomeNavbar from "../../components/navigation/HomeNavbar";
 import Footer from "../../components/Footer";
+import ProfileDisplay from "../../components/forms/ProfileDisplay";
 
 const Profile: NextPage = () => {
   const { data: sessionData } = useSession();
@@ -45,82 +46,10 @@ const Profile: NextPage = () => {
                 First Name Last Name
               </h1>
             </div>
-            <div className="mt-8 h-full w-full p-4">
-              <form className="mx-auto grid grid-cols-2 rounded-lg border border-gray-300 bg-white p-8 shadow-2xl">
-                <div className="col-span-2 mb-4 grid h-full w-full grid-cols-2">
-                  <div className="col-span-1 px-4">
-                    <label className="font-bold text-gray-900" htmlFor="name">
-                      Display Name
-                    </label>
-                    <input
-                      readOnly={true}
-                      type="text"
-                      name="displayName"
-                      value={
-                        sessionData?.user?.name
-                          ? `${sessionData.user?.name}`
-                          : "Degree Door User"
-                      }
-                      id="displayName"
-                      className="mt-2 w-full rounded-lg border border-gray-400 bg-slate-50 p-2 text-gray-900 outline-none
-                    duration-300 hover:shadow-2xl"
-                    />
-                  </div>
-                  <div className="col-span-1 px-4">
-                    <label className="font-bold text-gray-900" htmlFor="email">
-                      Email Address
-                    </label>
-                    <input
-                      readOnly={true}
-                      type="text"
-                      name="email"
-                      value={
-                        sessionData?.user?.email
-                          ? `${sessionData.user?.email}`
-                          : "..."
-                      }
-                      id="email"
-                      className="mt-2 w-full rounded-lg border border-gray-400 bg-slate-50 p-2 text-gray-900 outline-none
-                    duration-300 hover:shadow-2xl"
-                    />
-                  </div>
-                </div>
-                <div className="col-span-1 mb-4 px-4">
-                  <label htmlFor="status" className="font-bold text-gray-900">
-                    Status
-                  </label>
-                  <select
-                    id="status"
-                    name="status"
-                    className="mt-2 w-full rounded-lg border border-gray-400 bg-slate-50 p-2 text-gray-900 outline-none
-                  duration-300 hover:shadow-2xl"
-                  >
-                    <option value="Upcoming Student">Upcoming Student</option>
-                    <option value="Freshman">Freshman</option>
-                    <option value="Sophomore">Sophomore</option>
-                    <option value="Junior">Junior</option>
-                    <option value="Senior">Senior</option>
-                    <option value="Graduate">Graduate</option>
-                    <option value="Alumni">Alumni</option>
-                    <option value="Professor">Professor</option>
-                  </select>
-                </div>
-                <div className="col-span-2 px-4">
-                  <label htmlFor="about" className="font-bold text-gray-900">
-                    About Me
-                  </label>
-                  <textarea
-                    readOnly={true}
-                    name="about"
-                    value="about"
-                    id="about"
-                    className="mt-2 h-40 w-full resize-none rounded-lg 
-                    border border-gray-400 bg-slate-50 p-2 text-gray-900 
-                    outline-none duration-300 hover:shadow-2xl"
-                  />
-                </div>
-              </form>
-            </div>
+            <ProfileDisplay
+              displayName={sessionData?.user?.name}
+              email={sessionData?.user?.email}
+            />
           </section>
           <AuthShowcase />
         </main>
