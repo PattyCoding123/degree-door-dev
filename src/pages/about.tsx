e163713db2377c3736a5d216f9eead440b8f161a
Patrick <patrickducusin2@gmail.com>
2023-03-10 23:17:47 -0500

    Create new About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Degree Door About Page</title>
+        <meta name="description" content="The Degree Door About page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default About;
