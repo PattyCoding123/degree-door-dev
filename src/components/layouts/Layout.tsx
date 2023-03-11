4203a0edb68dc5e89702517d0556f7e27c4d238a
Patrick <patrickducusin2@gmail.com>
2023-03-11 17:37:11 -0500

    Create Layout for regular pages

---
+++ src/components/layouts/Layout.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import HomeNavbar from "../navigation/HomeNavbar";
+
+export interface LayoutProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ title, description, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HomeNavbar />
+      <div
+        className="min-w-screen relative min-h-screen bg-gradient-to-r
+     from-rose-400 via-fuchsia-500 to-indigo-500 pb-footer-fit-small text-white md:pb-footer-fit"
+      >
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Layout;
