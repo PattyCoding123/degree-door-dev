afb9e97eb33df1a41af9e267e455365ea7589d39
PattyCoding123 <patrickducusin2@gmail.com>
2023-03-14 15:53:56 -0400

    Make Navbar only render links if ID is available

--- src/components/navigation/DegreeNavbar.tsx
+++ src/components/navigation/DegreeNavbar.tsx
@@ -6,8 +6,8 @@ import Dropdown from "../menus/Dropdown";
 import FavoriteIndicator from "../FavoriteIndicator";
 interface DegreeNavbarProps {
   active: "overview" | "reviews" | "write";
-  degreeName: string;
-  degreeId: string;
+  degreeName?: string;
+  degreeId?: string;
 }
 
 const DegreeNavbar: React.FC<DegreeNavbarProps> = ({
@@ -36,40 +36,43 @@ const DegreeNavbar: React.FC<DegreeNavbarProps> = ({
         </Link>
         <ul className="flex flex-col items-center justify-center gap-10 md:col-span-1 md:flex-row md:gap-4">
           {/* Only render the links if the data exists */}
-          {[
-            {
-              href: `/degree/${degreeId}`,
-              label: "OVERVIEW",
-              id: "overview",
-            },
-            {
-              href: `/degree/${degreeId}/reviews`,
-              label: "REVIEWS",
-              id: "reviews",
-            },
-            {
-              href: `/degree/${degreeId}/write`,
-              label: "WRITE A REVIEW",
-              id: "write",
-            },
-          ].map(({ href, label, id }) => {
-            return (
-              <li key={label} id={id}>
-                <Link href={href}>
-                  <p
-                    className={clsx("font-bold hover:opacity-50", {
-                      "text-indigo-600": active === id,
-                    })}
-                  >
-                    {label}
-                  </p>
-                </Link>
-              </li>
-            );
-          })}
-          <li>
-            <FavoriteIndicator degreeId={degreeId} />
-          </li>
+          {degreeId &&
+            [
+              {
+                href: `/degree/${degreeId}`,
+                label: "OVERVIEW",
+                id: "overview",
+              },
+              {
+                href: `/degree/${degreeId}/reviews`,
+                label: "REVIEWS",
+                id: "reviews",
+              },
+              {
+                href: `/degree/${degreeId}/write`,
+                label: "WRITE A REVIEW",
+                id: "write",
+              },
+            ].map(({ href, label, id }) => {
+              return (
+                <li key={label} id={id}>
+                  <Link href={href}>
+                    <p
+                      className={clsx("font-bold hover:opacity-50", {
+                        "text-indigo-600": active === id,
+                      })}
+                    >
+                      {label}
+                    </p>
+                  </Link>
+                </li>
+              );
+            })}
+          {degreeId && (
+            <li>
+              <FavoriteIndicator degreeId={degreeId} />
+            </li>
+          )}
         </ul>
         <div className="flex justify-end md:col-span-1">
           <Dropdown color="black" />
