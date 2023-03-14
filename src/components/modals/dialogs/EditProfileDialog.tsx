ab4465c66db0ea962e5b2c258817aeb51c9e2d80
Patrick Ducusin <83043144+PattyCoding123@users.noreply.github.com>
2023-03-14 17:32:50 -0400

    Include clickOutside ref

--- src/components/modals/dialogs/EditProfileDialog.tsx
+++ src/components/modals/dialogs/EditProfileDialog.tsx
@@ -1,3 +1,4 @@
+import useClickOutside from "../../../utils/useOutsideClick";
 import { type ProfileDisplayProps } from "../../forms/ProfileDisplay";
 import Modal from "../Modal";
 
@@ -12,11 +13,13 @@ const EditProfileDialog: React.FC<EditProfileDialogProps> = ({
   handleCancel,
   show,
 }) => {
+  const ref = useClickOutside(() => handleCancel());
+
   return (
     <>
       {show && (
         <Modal>
-          <div className="mt-8 h-full w-full p-4">
+          <div ref={ref} className="mt-8 h-full w-full p-4">
             <form className="mx-auto grid grid-cols-2 rounded-lg border border-gray-300 bg-white p-8 shadow-2xl">
               <div className="col-span-2 mb-4 grid h-full w-full grid-cols-2">
                 <div className="col-span-1 px-4">
