7004bf039970e8c82949510f77c7e8cf2c426607
Patrick Ducusin <83043144+PattyCoding123@users.noreply.github.com>
2023-03-15 14:35:41 -0400

    Increase the size of the GeneralLoadingIndicator

--- src/components/loading-ui/GeneralLoadingIndicator.tsx
+++ src/components/loading-ui/GeneralLoadingIndicator.tsx
@@ -2,7 +2,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const GeneralLoadingIndicator: React.FC = () => {
   return (
-    <AiOutlineLoading3Quarters className="animate-spin text-4xl font-bold" />
+    <AiOutlineLoading3Quarters className="animate-spin text-5xl font-bold" />
   );
 };
 
