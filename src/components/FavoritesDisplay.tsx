dc5d450481a46151f800d1568b064864b143168b
Patrick <patrickducusin2@gmail.com>
2023-03-15 21:10:43 -0400

    Fix isError branch for rendering component

--- src/components/FavoritesDisplay.tsx
+++ src/components/FavoritesDisplay.tsx
@@ -5,25 +5,37 @@ import { Button } from "./Buttons";
 import GeneralLoadingIndicator from "./loading-ui/GeneralLoadingIndicator";
 import FavoriteDegree from "./FavoriteDegree";
 
+// A component that will render various UI depending
+// on the status of the session or favorites.
 const FavoritesDisplay: React.FC = () => {
   const { data: sessionData } = useSession();
   const favorites = trpc.forum.getFavorites.useQuery(undefined, {
     enabled: sessionData?.user !== undefined,
   });
 
+  // Render Log in message if the user's isn't logged in
   if (!sessionData || !sessionData.user) {
-    return <p className="mt-4 text-xl">Log in to start favoriting degrees!</p>;
+    return (
+      <p className="mt-16 text-2xl">Log in to start favoriting degrees!</p>
+    );
   }
 
+  // Render general loading indicator if favorites is fetching
   if (favorites.isFetching) {
     return <GeneralLoadingIndicator size="extra-large" />;
   }
 
+  // Render an error message and button to refetch the queries IF
+  // there was an error trying to fetch the favorites.
   if (favorites.isError) {
     return (
-      <div className="flex flex-col">
-        <p>There was a problem fetching your favorite degrees...</p>
-        <Button onClick={() => favorites.refetch()}>Retry</Button>
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-2xl">
+          There was a problem fetching your favorite degrees...
+        </p>
+        <Button className="w-24" onClick={() => favorites.refetch()}>
+          Retry
+        </Button>
       </div>
     );
   }
