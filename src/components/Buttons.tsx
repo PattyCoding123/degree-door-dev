6e1ef7eb9be616df7b9fe72e830900b3bc6a4221
Patrick <patrickducusin2@gmail.com>
2023-03-15 23:42:21 -0400

    Remove default color from Button

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,11 +1,11 @@
 export function Button(props: React.ComponentPropsWithoutRef<"button">) {
   const activeButton =
-    "inline-block px-6 py-2.5 bg-green-600 text-white font-medium cursor-pointer " +
+    "inline-block px-6 py-2.5 text-white font-medium cursor-pointer " +
     "text-sm leading-tight uppercase rounded shadow-md hover:opacity-80 hover:shadow-lg active:scale-90 " +
     "duration-100 ease-in-out";
 
   const disabledButton =
-    "inline-block px-6 py-2.5 bg-green-600 text-white font-medium " +
+    "inline-block px-6 py-2.5 text-white font-medium " +
     "text-sm leading-tight uppercase rounded shadow-md opacity-50";
 
   const { className, disabled, ...rest } = props;
