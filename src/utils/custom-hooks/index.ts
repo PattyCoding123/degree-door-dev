a91cac73102cdcbbc28427e624eaffa4bdcf3f07
Patrick <patrickducusin2@gmail.com>
2023-03-15 23:56:46 -0400

    Remove useIsFavorite

--- src/utils/custom-hooks/index.ts
+++ src/utils/custom-hooks/index.ts
@@ -3,4 +3,3 @@ export { default as useDeleteReview } from "./useDeleteReview";
 export { default as useReviewQuery } from "./useReviewQuery";
 export { default as usePrevious } from "./usePrevious";
 export { default as useOutsideClick } from "./useOutsideClick";
-export { default as useIsFavorite } from "./useIsFavorite";
