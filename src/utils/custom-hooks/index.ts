04a2fe40d2cb5ab6a95148aa115e50714f3768ef
Patrick <patrickducusin2@gmail.com>
2023-03-15 22:09:44 -0400

    Export all custom hooks

---
+++ src/utils/custom-hooks/index.ts
@@ -0,0 +1,5 @@
+export { default as useDegreeQuery } from "./useDegreeQuery";
+export { default as useDeleteReview } from "./useDeleteReview";
+export { default as useReviewQuery } from "./useReviewQuery";
+export { default as usePrevious } from "./usePrevious";
+export { default as useOutsideClick } from "./useOutsideClick";
