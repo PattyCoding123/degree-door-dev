06a9ed194d7d2dd346a5290c6bde63bbc3ae7813
Patrick <patrickducusin2@gmail.com>
2023-03-16 20:30:27 -0400

    Create mutation procedure to edit profile information

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const authRouter = router({
@@ -7,4 +8,26 @@ export const authRouter = router({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+  editProfile: protectedProcedure
+    .input(
+      z.object({
+        displayName: z.string().nullish(),
+        status: z.string(),
+        about: z.string().nullish(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      // * Update user's name, status, and about if changed.
+      const userInfo = await ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          name: input.displayName,
+          status: input.status,
+          about: input.about,
+        },
+      });
+      return userInfo;
+    }),
 });
