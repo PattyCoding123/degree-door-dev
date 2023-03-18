44e6ff10ca1738ac781cdeb092e6d46edbc06d80
Patrick <patrickducusin2@gmail.com>
2023-03-18 00:26:51 -0400

    Rename assertion function

--- src/utils/assertIsReviewAuthor.ts
+++ src/utils/assertIsReviewAuthor.ts
@@ -1,7 +1,7 @@
 import { TRPCError } from "@trpc/server";
 import { type Context } from "../server/trpc/context";
 
-export const assertIsReviewWriter = async (ctx: Context, reviewId: string) => {
+export const assertIsReviewAuthor = async (ctx: Context, reviewId: string) => {
   const review = await ctx.prisma.review.findUnique({
     where: { id: reviewId },
   });
