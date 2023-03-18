cde397129a68fab6664d7d9ba4fc40209acbe4c5
Patrick <patrickducusin2@gmail.com>
2023-03-17 22:31:15 -0400

    Remove reviews handling into ReviewsDisplay

---
+++ src/components/containers/ReviewsDisplay.tsx
@@ -0,0 +1,93 @@
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+import { useDeleteReview, useReviewQuery } from "../../utils/custom-hooks";
+import { Button } from "../Buttons";
+import Review from "../Review";
+import GeneralLoadingIndicator from "../loading-ui/GeneralLoadingIndicator";
+import ConfirmationDialog from "../modals/dialogs/ConfirmationDialog";
+
+interface ReviewsDisplayProps {
+  enableQuery: boolean;
+  degreeId: string;
+}
+
+const ReviewsDisplay: React.FC<ReviewsDisplayProps> = ({
+  enableQuery,
+  degreeId,
+}) => {
+  const { data: sessionData } = useSession();
+  const [selectedReview, setSelectedReview] = useState<string>(); // To determine which
+  const [showDialog, setShowDialog] = useState(false); // State to control dialog
+  const reviewsQuery = useReviewQuery(enableQuery, degreeId);
+
+  // Procedure to delete a review for that degree forum.
+  const deleteReview = useDeleteReview(degreeId);
+
+  if (reviewsQuery.isFetching || reviewsQuery.isLoading) {
+    return (
+      <div className="flex flex-col items-center">
+        <GeneralLoadingIndicator size="extra-large" />
+      </div>
+    );
+  }
+
+  if (reviewsQuery.isError) {
+    return (
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-2xl">There was a problem fetching the reviews...</p>
+        <Button
+          className="w-24 bg-red-500"
+          onClick={() => reviewsQuery.refetch()}
+        >
+          Retry
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {/* The confirmation dialog */}
+      <ConfirmationDialog
+        header="Delete your Review"
+        content="Are you sure you want to delete this review? It cannot be recovered after."
+        handleOk={async () => {
+          if (typeof selectedReview === "string") {
+            // Delete review if the selectedReview state is not undefined
+            deleteReview.mutateAsync({ reviewId: selectedReview });
+          }
+          setSelectedReview(undefined); // Reset selectedReview state
+          setShowDialog(false); // Remove ConfirmationDialog
+        }}
+        handleCancel={() => {
+          setSelectedReview(undefined); // Reset selectedReview state
+          setShowDialog(false); // Remove ConfirmationDialog
+        }} // close dialog
+        show={showDialog}
+        okBtnText="Delete"
+      />
+      {reviewsQuery.data.length > 0 ? (
+        reviewsQuery.data.map((review) => (
+          // ! Review handleClick will set the selectedReviewState and render the ConfirmationDialog
+          // ! Reviews are only deletable by authenticated authors and if the userIds match.
+          <Review
+            canDelete={sessionData?.user?.id === review.userId}
+            key={review.id}
+            reviewPost={review}
+            handleClick={() => {
+              setSelectedReview(review.id);
+              setShowDialog(true);
+            }}
+          />
+        ))
+      ) : (
+        <p className="mt-4 text-xl">
+          This forum has no degrees. Be the first to post a review!
+        </p>
+      )}
+    </>
+  );
+};
+
+export default ReviewsDisplay;
--- src/pages/degree/[degree]/reviews.tsx
+++ src/pages/degree/[degree]/reviews.tsx
@@ -1,37 +1,20 @@
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
-import { useState } from "react";
 
-import Review from "../../../components/Review";
 import ForumLayout from "../../../components/layouts/ForumLayout";
-import ConfirmationDialog from "../../../components/modals/dialogs/ConfirmationDialog";
-import {
-  useDegreeQuery,
-  useReviewQuery,
-  useDeleteReview,
-} from "../../../utils/custom-hooks";
+import { useDegreeQuery } from "../../../utils/custom-hooks";
+import ReviewsDisplay from "../../../components/containers/ReviewsDisplay";
 
 // The Reviews page will render all the reviews for a specific degree forum.
 // It will also allow user's to delete reviews if they are the author
 const ReviewsPage: NextPage = () => {
-  const { data: sessionData } = useSession(); // Get session data
-  const [showDialog, setShowDialog] = useState(false); // State to control dialog
-  const [selectedReview, setSelectedReview] = useState<string>(); // To determine which
-
   const router = useRouter();
   const { degree } = router.query;
 
   // Dependent query, will not run unless degree is defined
   // Push to page /404 if degree info is not found
   const degreeQuery = useDegreeQuery(degree);
-
-  // Only query for reviews if the degreeQuery is successful
-  const reviewsQuery = useReviewQuery(degreeQuery.isSuccess, degree as string);
-
-  // Procedure to delete a review for that degree forum.
-  const deleteReview = useDeleteReview(degree);
 
   return (
     <ForumLayout
@@ -41,25 +24,6 @@ const ReviewsPage: NextPage = () => {
       degreeName={degreeQuery.data?.name}
       active="reviews"
     >
-      {/* The confirmation dialog */}
-      <ConfirmationDialog
-        header="Delete your Review"
-        content="Are you sure you want to delete this review? It cannot be recovered after."
-        handleOk={async () => {
-          if (typeof selectedReview === "string") {
-            // Delete review if the selectedReview state is not undefined
-            deleteReview.mutateAsync({ reviewId: selectedReview });
-          }
-          setSelectedReview(undefined); // Reset selectedReview state
-          setShowDialog(false); // Remove ConfirmationDialog
-        }}
-        handleCancel={() => {
-          setSelectedReview(undefined); // Reset selectedReview state
-          setShowDialog(false); // Remove ConfirmationDialog
-        }} // close dialog
-        show={showDialog}
-        okBtnText="Delete"
-      />
       <Toaster />
       {/* Toaster component for rendering react-hot-toast messages */}
       {degreeQuery.isSuccess && (
@@ -75,19 +39,7 @@ const ReviewsPage: NextPage = () => {
             <p className="text-xl md:text-3xl">Reviews</p>
           </div>
           <section className="mt-16 mb-8 flex flex-col items-center justify-center gap-8">
-            {reviewsQuery.data?.map((review) => (
-              // ! Review handleClick will set the selectedReviewState and render the ConfirmationDialog
-              // ! Reviews are only deletable by authenticated authors and if the userIds match.
-              <Review
-                canDelete={sessionData?.user?.id === review.userId}
-                key={review.id}
-                reviewPost={review}
-                handleClick={() => {
-                  setSelectedReview(review.id);
-                  setShowDialog(true);
-                }}
-              />
-            ))}
+            <ReviewsDisplay enableQuery={true} degreeId={degreeQuery.data.id} />
           </section>
         </main>
       )}
